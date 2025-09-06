2560df79fd5070cac34480d403fb0810d9019864
Lavtsf <learnlkora@gmail.com>
2025-09-07 01:36:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,15 @@ st.write(
 
 # st.write('Your Favorite Fruit is: ', fruit_option)
 
-session = get_active_session()
+#session = get_active_session()
 # st.write(session)
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
+cnx = st.connection("snowflake")
+session = cnx.session()
+
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name in your Smoothie will be: ", name_on_order)
